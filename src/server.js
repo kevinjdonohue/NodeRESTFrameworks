b9b615636905577fd943a61dca4e780861794ce5
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-06-18 22:47:23 -0700

    experimenting with bunyan logger

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 const restify = require('restify');
+const bunyan = require('bunyan');
 
 const htmlFormatter = (request, response, body) => {
   if (body instanceof Error) {
@@ -19,6 +20,12 @@ const server = restify.createServer({
   },
   strictNext: true,
   ignoreTrailingSlash: true,
+  log: bunyan.createLogger({
+    name: 'sample-restify-logger',
+    level: 'debug',
+    stream: process.stdout,
+    src: true,
+  }),
 });
 
 server.listen(8080, 'localhost', () => {
@@ -27,23 +34,39 @@ server.listen(8080, 'localhost', () => {
 
 // pre-handlers -- handler chain is executed before routing
 
-// const preHandler = (request, response, next) => {
-//   console.log('Fired pre-handler via server.pre');
-//   return next();
-// };
-
-// server.pre(preHandler);
+const preHandler = (request, response, next) =>
+  // console.log('Fired pre-handler via server.pre');
+  // request.log.info('Pre-handler fired via server.pre');
+  next();
+server.pre(preHandler);
 
 // universal handler - server.use - handler chain is executed after a route has been chosen
 
-// server.use((request, response, next) => {
-//   console.log('Fired universal handler via server.use');
-//   // console.log('response.status: ', response.statusCode);
-//   return next();
-// });
+server.use((request, response, next) =>
+  // console.log('Fired universal handler via server.use');
+  // console.log('response.status: ', response.statusCode);
+  // request.log.info('Universal handler fired via server.use');
+  next());
 
 // can execute an array of functions via server.use
 // server.use([function foo () {}, function bar () {}])
+
+// server.use(restify.plugins.requestLogger());
+
+server.on(
+  'after',
+  restify.plugins.auditLogger({
+    log: bunyan.createLogger({
+      name: 'audit',
+      level: 'debug',
+      stream: process.stdout,
+      src: true,
+    }),
+    event: 'after',
+    server,
+    printLog: true,
+  }),
+);
 
 server.on('NotFound', (request, response, error, callback) => {
   console.log('in the NotFound handler!');
